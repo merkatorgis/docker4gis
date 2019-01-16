89a60df18718fc1569eb61d54aef0a2160d5d11a
Wouter Scherphof <wouter.scherphof@gmail.com>
2019-01-16 14:26:27 +0100

    rename on build for glassfish & serve
    stop (and rename) running container so that after build, a new container from the new image is run instead of the existing old container from the old image.
    
    this is as with the other images, it just wasn't yet done for glassfish (api) & serve (app)

--- base/glassfish/build.sh
+++ base/glassfish/build.sh
@@ -7,12 +7,11 @@ DOCKER_REGISTRY="${DOCKER_REGISTRY}"
 DOCKER_USER="${DOCKER_USER:-merkatorgis}"
 DOCKER_REPO="${DOCKER_REPO:-api}"
 DOCKER_TAG="${DOCKER_TAG:-latest}"
+API_CONTAINER="${API_CONTAINER:-$DOCKER_USER-api}"
 
 IMAGE="${DOCKER_REGISTRY}${DOCKER_USER}/${DOCKER_REPO}:${DOCKER_TAG}"
 
 echo; echo "Building $IMAGE"
-
-HERE=$(dirname "$0")
 
 echo; echo "Compiling from '${src_dir}'..."
 
@@ -29,6 +28,9 @@ echo; echo "Building server from binaries..."
 read -r -a artifact_id <<< $(grep -oPm1 '(?<=<artifactId>)[^<]+' "${src_dir}/pom.xml")
 read -r -a version <<< $(grep -oPm1 '(?<=<version>)[^<]+' "${src_dir}/pom.xml")
 build_dir="${src_dir}/target/${artifact_id}-${version}"
+
+HERE=$(dirname "$0")
+"$HERE/../rename.sh" "$IMAGE" "$API_CONTAINER" force
 
 # Asserting a ./Dockerfile like:
 # FROM aws-eb-glassfish:5.0-al-onbuild-2.11.1
--- base/serve/build.sh
+++ base/serve/build.sh
@@ -7,10 +7,14 @@ DOCKER_REGISTRY="${DOCKER_REGISTRY}"
 DOCKER_USER="${DOCKER_USER:-merkatorgis}"
 DOCKER_REPO="${DOCKER_REPO:-app}"
 DOCKER_TAG="${DOCKER_TAG:-latest}"
+APP_CONTAINER="${APP_CONTAINER:-$DOCKER_USER-app}"
 
 IMAGE="${DOCKER_REGISTRY}${DOCKER_USER}/${DOCKER_REPO}:${DOCKER_TAG}"
 
 echo; echo "Building $IMAGE"
+
+HERE=$(dirname "$0")
+"$HERE/../rename.sh" "$IMAGE" "$APP_CONTAINER" force
 
 pushd "${build_dir}"
 
