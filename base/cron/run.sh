3fe83e7be44d69d3c00e6d86bebe6c210954b70b
Wouter Scherphof <wouter.scherphof@gmail.com>
2019-04-25 16:51:47 +0200

    leftover from last fix

--- base/cron/run.sh
+++ base/cron/run.sh
@@ -15,7 +15,7 @@ container="${CRON_CONTAINER:-$DOCKER_USER-cr}"
 image="${DOCKER_REGISTRY}${DOCKER_USER}/${DOCKER_REPO}:${DOCKER_TAG}"
 here=$(dirname "$0")
 
-if "$here/../start.sh" "${container}"; then exit; fi
+if "$here/../start.sh" "${image}" "${container}"; then exit; fi
 
 mkdir -p "${DOCKER_BINDS_DIR}/secrets"
 mkdir -p "${DOCKER_BINDS_DIR}/fileport"
