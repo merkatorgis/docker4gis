d472915c250f240217abd950855a9c3e7cd9bf69
Wouter Scherphof <wouter.scherphof@gmail.com>
2019-11-02 21:11:34 +0100

    postgrest dynamified

--- base/postgis/conf/auth/1.sh
+++ base/postgis/conf/auth/1.sh
@@ -5,8 +5,8 @@ pushd schema/1
 
 # select current_setting('app.jwt_secret');
 # needs at least 32 characters
-jwt_secret="$(pg.sh -c 'select gen_random_uuid()' -At).$(pg.sh -c 'select gen_random_uuid()' -At)"
-pg.sh -c "ALTER DATABASE ${POSTGRES_DB} SET app.jwt_secret TO '${jwt_secret}'"
+PGRST_JWT_SECRET="$(pg.sh -Atc 'select gen_random_uuid()' -At).$(pg.sh -c 'select gen_random_uuid()' -At)"
+pg.sh -c "ALTER DATABASE ${POSTGRES_DB} SET app.jwt_secret TO '${PGRST_JWT_SECRET}'"
 
 pg.sh -f jwt_token.sql
 pg.sh -f fn_jwt_time.sql
--- base/postgrest/run.sh
+++ base/postgrest/run.sh
@@ -13,7 +13,12 @@ image="${DOCKER_REGISTRY}${DOCKER_USER}/${repo}:${DOCKER_TAG}"
 
 if .run/start.sh "${image}" "${container}"; then exit; fi
 
+PGRST_JWT_SECRET=$(docker container exec "${DOCKER_USER}-postgis" pg.sh force -Atc "select current_setting('app.jwt_secret')")
+
 docker run --name "${container}" \
 	--network "${DOCKER_USER}-net" \
+	-e PGRST_DB_URI=postgresql://authenticator:postgrest@${DOCKER_USER}-postgis/${DOCKER_USER} \
+	-e PGRST_DB_SCHEMA=${DOCKER_USER} \
+	-e PGRST_JWT_SECRET=${PGRST_JWT_SECRET} \
 	"$@" \
 	-d "${image}"
