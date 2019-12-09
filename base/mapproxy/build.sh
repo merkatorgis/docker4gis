952c110866a5aeef7d335140a0e3d364e976067e
Wouter Scherphof <wouter.scherphof@gmail.com>
2019-12-09 17:29:09 +0100

    first working mapproxy
    demo service not running behind proxy :-(

--- base/mapproxy/build.sh
+++ base/mapproxy/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -x #echo on
 
 DOCKER_REGISTRY="${DOCKER_REGISTRY}"
-DOCKER_USER="${DOCKER_USER:-mapservices}"
+DOCKER_USER="${DOCKER_USER:-docker4gis}"
 
 repo=$(basename "$(pwd)")
 container="${DOCKER_USER}-${repo}"
@@ -13,4 +12,7 @@ docker container rm -f "${container}" 2>/dev/null
 
 HERE=$(dirname "$0")
 
+mkdir -p conf
+cp -r "${HERE}/../plugins" "conf"
 docker image build -t "${image}" .
+rm -rf "conf/plugins"
