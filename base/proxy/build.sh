974919afbc351faeb1fb0753febd0d77be613ae5
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-01-09 12:16:15 +0100

    proxy bit simplified no changes in actual functionality

--- base/proxy/build.sh
+++ base/proxy/build.sh
@@ -12,10 +12,10 @@ docker container rm -f "${container}" 2>/dev/null
 
 HERE=$(dirname "$0")
 
-if [ -d ./goproxy ]; then # building base
+if [ -d goproxy ]; then # building base
 	if (which go 1>/dev/null 2>&1); then
 
-		cd ./goproxy
+		pushd goproxy
 		export MSYS_NO_PATHCONV=1
 		docker container run --rm \
 			-v "$PWD":/usr/src/goproxy \
@@ -24,7 +24,7 @@ if [ -d ./goproxy ]; then # building base
 			-e GOOS=linux \
 			golang \
 			go build -v -a -tags netgo -ldflags '-w' .
-		cd ..
+		popd
 
 		docker image build -t "${image}" .
 	else
