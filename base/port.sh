76222971cf25de045147cfcbd831efdf1e0128f1
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-01-15 08:46:13 +0100

    wip proxy: flexible ports

--- base/port.sh
+++ base/port.sh
@@ -5,5 +5,10 @@ container_port=$2
 
 if [ ${host_port} ]
 then
+	# If taken, increment port until it's free
+	while ! docker container run --rm -p "${host_port}":"${host_port}" alpine true 1>/dev/null 2>&1
+	do
+		host_port=$((host_port + 1))
+	done
 	echo "-p ${host_port}:${container_port}"
 fi
