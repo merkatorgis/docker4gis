54c6c889ed3cb27fc487488884099bea11200c6d
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-05-01 07:26:46 +0200

    typo in template

--- templates/serve/build.sh
+++ templates/serve/build.sh
@@ -2,6 +2,6 @@
 
 # To specify any directory to copy to the image & serve:
 # build_dir="$(pwd)/../../app"
-# If left empty, dynamic content is served from "${DOCKER_BINDS_DIR}/fileport${DOCKER_USER}"
+# If left empty, dynamic content is served from "${DOCKER_BINDS_DIR}/fileport/${DOCKER_USER}"
 
 "${DOCKER_BASE}/serve/build.sh" "${build_dir}" 
