42e2f0016af6f328bf61e216223be30f114d774c
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-07-20 23:34:50 +0200

    test.sh minor corrections

--- base/test.sh
+++ base/test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-dir="$1"
-if ! [ "$dir" ]; then
-    dir='test'
-    mkdir -p "$dir"
-fi
+dir="${1:-test}"
+
 if ! [ -d "$dir" ]; then
-    echo "ERROR - cannot find directory $dir"
-    exit 22
+    if [ "$dir" = "test" ]; then
+        echo "WARNING - consider adding some integration tests"
+        exit 0
+    else
+        echo "ERROR - cannot find directory $dir"
+        exit 22
+    fi
 fi
 
 echo "Running any tests in $dir..."
@@ -16,7 +18,7 @@ find "$dir" -name "test.sh" -exec {} \;
 
 if find "$dir" -name "*.bats" >/dev/null 2>&1; then
     "$DOCKER_BASE"/plugins/bats/install.sh
-    if ! command -v bats; then
+    if ! command -v bats >/dev/null 2>&1; then
         bats_url=https://github.com/bats-core/bats-core
         echo "WARNING - cannot test without [bats]($bats_url); continuing without running any tests now."
         exit
