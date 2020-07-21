c9c5b019791c2a3cda7b0e79f8ecf532f40c87ca
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-07-21 12:27:41 +0200

    bats: include plugin in base images

--- base/postgis/conf/safeupdate/install.sh
+++ base/postgis/conf/safeupdate/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 apk add --no-cache --virtual .build-deps \
-	py-setuptools make gcc libc-dev clang llvm9
+    py-setuptools make gcc libc-dev clang llvm5
 
 pgxn install safeupdate
 
 echo "shared_preload_libraries=safeupdate" \
-    >> /etc/postgresql/postgresql.conf
+    >>/etc/postgresql/postgresql.conf
 
 apk del .build-deps
