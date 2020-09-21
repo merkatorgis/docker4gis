dadd10c7419be34abf795560d67d95f06989e466
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-09-21 16:51:27 +0200

    contained postgis template

--- templates/postgis/conf/DOCKER_USER/1.sh
+++ templates/postgis/conf/DOCKER_USER/1.sh
@@ -3,21 +3,20 @@ set -e
 
 pushd schema/1
 
-    pg.sh -c "comment on schema ${SCHEMA} is
-        \$\$${SCHEMA}: Data on things\$\$"
+pg.sh -c "comment on schema $SCHEMA is
+        \$\$$SCHEMA: Data on things\$\$"
 
-    pg.sh -c "set search_path to ${SCHEMA}, public" \
-        -f things.sql
+pg.sh -c "set search_path to $SCHEMA, public" \
+    -f things.sql
 
-    # PostgREST stuff
-    pushd web
-        ./web.sh
-    popd
+# PostgREST stuff
+pushd web
+./web.sh
+popd
 
-    if [ ${DOCKER_ENV} != PRODUCTION ]
-    then
-        pg.sh -c "set search_path to ${SCHEMA}, public" \
-            -f testdata.sql
-    fi
+if [ "$DOCKER_ENV" != PRODUCTION ]; then
+    pg.sh -c "set search_path to $SCHEMA, public" \
+        -f testdata.sql
+fi
 
 popd
