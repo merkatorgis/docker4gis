1432e9001822b9d25467f7b3eeb6fa54fc1752f9
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-09-22 21:38:30 +0200

    contained: mapfish

--- base/mapfish/run.sh
+++ base/mapfish/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
-DOCKER_REGISTRY="${DOCKER_REGISTRY}"
-DOCKER_USER="${DOCKER_USER}"
-DOCKER_TAG="${DOCKER_TAG}"
-DOCKER_ENV="${DOCKER_ENV}"
-DOCKER_BINDS_DIR="${DOCKER_BINDS_DIR}"
+repo="$1"
+tag="$2"
+shift 2
 
-repo=$(basename "$0")
-container="${DOCKER_USER}-${repo}"
-image="${DOCKER_REGISTRY}${DOCKER_USER}/${repo}:${DOCKER_TAG}"
+DOCKER_REGISTRY="$DOCKER_REGISTRY"
+DOCKER_USER="$DOCKER_USER"
+DOCKER_ENV="$DOCKER_ENV"
+DOCKER_BINDS_DIR="$DOCKER_BINDS_DIR"
 
-if .run/start.sh "${image}" "${container}"; then exit; fi
+container="$DOCKER_USER"-"$repo"
+image="$DOCKER_REGISTRY""$DOCKER_USER"/"$repo":"$tag"
 
 XMS="${XMS:-256m}"
 XMX="${XMX:-2g}"
 
-docker container run --restart always --name "${container}" \
-	-e DOCKER_USER="${DOCKER_USER}" \
-	-e JAVA_OPTS="-Xms${XMS} -Xmx${XMX} -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:+UseParNewGC -XX:NewRatio=2 -XX:+AggressiveOpts" \
-	--network "${DOCKER_USER}" \
+docker container run --restart always --name "$container" \
+	-e DOCKER_USER="$DOCKER_USER" \
+	-e JAVA_OPTS="-Xms$XMS -Xmx$XMX -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:+UseParNewGC -XX:NewRatio=2 -XX:+AggressiveOpts" \
+	--network "$DOCKER_USER" \
 	"$@" \
-	-d "${image}"
+	-d "$image"
