133ccc97f578e3a2d210124c85916739ee4e1718
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-09-22 21:25:14 +0200

    contained: cron

--- base/cron/build.sh
+++ base/cron/build.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-DOCKER_REGISTRY="${DOCKER_REGISTRY}"
+DOCKER_BASE="$DOCKER_BASE"
+DOCKER_REGISTRY="$DOCKER_REGISTRY"
 DOCKER_USER="${DOCKER_USER:-docker4gis}"
 
 repo=$(basename "$(pwd)")
-container="${DOCKER_USER}-${repo}"
-image="${DOCKER_REGISTRY}${DOCKER_USER}/${repo}"
-
-echo; echo "Building $image"
-docker container rm -f "${container}" 2>/dev/null
-
-HERE=$(dirname "$0")
+image="$DOCKER_REGISTRY""$DOCKER_USER"/"$repo"
 
 mkdir -p conf
-cp -r "${HERE}/../plugins" "conf"
-docker image build -t "${image}" .
-rm -rf "conf/plugins"
+cp -r "$DOCKER_BASE"/plugins "$DOCKER_BASE"/.docker4gis conf
+docker image build \
+    -t "$image" .
+rm -rf conf/plugins conf/.docker4gis
--- templates/cron/build.sh
+++ templates/cron/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-"${DOCKER_BASE}/cron/build.sh"
+"$BASE"
