95aef5050e90a49d0dc77f22a42edc144a9ae788
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-09-24 20:21:19 +0200

    contained: postfix

--- base/postfix/run.sh
+++ base/postfix/run.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 set -e
 
-DOCKER_REGISTRY="${DOCKER_REGISTRY}"
-DOCKER_USER="${DOCKER_USER}"
-DOCKER_TAG="${DOCKER_TAG}"
-DOCKER_ENV="${DOCKER_ENV}"
-DOCKER_BINDS_DIR="${DOCKER_BINDS_DIR}"
+repo="$1"
+tag="$2"
+shift 2
 
-repo=$(basename "$0")
-container="${DOCKER_USER}-${repo}"
-image="${DOCKER_REGISTRY}${DOCKER_USER}/${repo}:${DOCKER_TAG}"
+DOCKER_REGISTRY="$DOCKER_REGISTRY"
+DOCKER_USER="$DOCKER_USER"
+DOCKER_ENV="$DOCKER_ENV"
+DOCKER_BINDS_DIR="$DOCKER_BINDS_DIR"
 
-POSTFIX_DESTINATION="${POSTFIX_DESTINATION}"
+container="$DOCKER_USER"-"$repo"
+image="$DOCKER_REGISTRY""$DOCKER_USER"/"$repo":"$tag"
 
-if .run/start.sh "${image}" "${container}"; then exit; fi
+POSTFIX_DESTINATION="$POSTFIX_DESTINATION"
 
-destination=
-if [ "${POSTFIX_DESTINATION}" != '' ]; then
-	destination="-e DESTINATION=${POSTFIX_DESTINATION}"
-fi
+POSTFIX_PORT=$(docker4gis/port.sh "${POSTFIX_PORT:-25}")
 
-POSTFIX_PORT=$(.run/port.sh "${POSTFIX_PORT:-25}")
-
-docker container run --restart always --name $container \
-	-e DOCKER_USER="${DOCKER_USER}" \
-	-v "$(docker_bind_source "${DOCKER_BINDS_DIR}/fileport")":/fileport \
-	-v "$(docker_bind_source "${DOCKER_BINDS_DIR}/runner")":/util/runner/log \
-	-p "${POSTFIX_PORT}":25 \
-	${destination} \
-	--network "${DOCKER_USER}" \
-	-d $image
+docker container run --restart always --name "$container" \
+	-e DOCKER_USER="$DOCKER_USER" \
+	-v "$(docker4gis/bind.sh "$DOCKER_BINDS_DIR"/fileport /fileport)" \
+	-v "$(docker4gis/bind.sh "$DOCKER_BINDS_DIR"/runner /util/runner/log)" \
+	-p "$POSTFIX_PORT":25 \
+	-e "$(docker4gis/noop.sh DESTINATION "$POSTFIX_DESTINATION")" \
+	--network "$DOCKER_USER" \
+	-d "$image"
