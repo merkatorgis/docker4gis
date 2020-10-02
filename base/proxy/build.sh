301d36a7ea7c6bd1b30fec11e3f00c30e340a715
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-10-02 15:33:36 +0200

    proxy: multistage build

--- base/proxy/build.sh
+++ base/proxy/build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-[ "$IMAGE" ] || base=true
 IMAGE=${IMAGE:-docker4gis/$(basename "$(realpath .)")}
 DOCKER_BASE=$DOCKER_BASE
+DOCKER_USER=$DOCKER_USER
 
 mkdir -p conf
 cp -r "$DOCKER_BASE"/.plugins "$DOCKER_BASE"/.docker4gis conf
-if [ "$base" ]; then
-	goproxy/builder/run.sh &&
-		docker image build -t "$IMAGE" .
-else
-	docker image build \
-		--build-arg DOCKER_USER="$DOCKER_USER" \
-		-t "$IMAGE" .
-fi
+docker image build \
+	--build-arg DOCKER_USER="$DOCKER_USER" \
+	-t "$IMAGE" .
 rm -rf conf/.plugins conf/.docker4gis
