564c784990cfc65f6aef8d7ab5efe57b44b564a0
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-10-26 13:34:04 +0100

    registry: use autocert

--- base/registry/build.sh
+++ base/registry/build.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 set -e
 
-[ "$IMAGE" ] || base=true
-IMAGE=${IMAGE:-docker4gis/$(basename "$(realpath .)")}
-DOCKER_BASE=$DOCKER_BASE
-
-if [ "$base" ]; then
-	which go
-	cd ./goproxy
-	CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags '-w' .
-	cd ..
-	docker build -t "$IMAGE" .
-else
-	docker build -t "$IMAGE" .
-fi
+docker build -t docker4gis/registry .
--- base/registry/run.sh
+++ base/registry/run.sh
@@ -2,32 +2,24 @@
 set -e
 export MSYS_NO_PATHCONV=1
 
+tag=${1:-latest}
 REGISTRY_HOST=${REGISTRY_HOST:-localhost}
 REGISTRY_PORT=${REGISTRY_PORT:-443}
-
-DOCKER_REGISTRY=${DOCKER_REGISTRY}
-DOCKER_USER=${DOCKER_USER}
-DOCKER_TAG=${DOCKER_TAG}
-DOCKER_ENV=${DOCKER_ENV}
-DOCKER_BINDS_DIR=${DOCKER_BINDS_DIR}
-
-repo=$(basename "$0")
-container=${DOCKER_USER}-${repo}
-image=${DOCKER_REGISTRY}${DOCKER_USER}/${repo}:${DOCKER_TAG}
-
-if .run/start.sh "${image}" "${container}"; then exit; fi
+AUTOCERT=${AUTOCERT:-false}
+DOCKER_ENV=${DOCKER_ENV:-DEVELOPMENT}
 
 docker volume create certificates
 docker volume create registry
 if (docker network create registry 2>/dev/null); then true; fi
 
-docker container run --restart always --name "$container" \
+docker container run --restart always --name registry \
 	--network registry \
 	-v certificates:/certificates \
 	-e REGISTRY_HOST="$REGISTRY_HOST" \
+	-e AUTOCERT="$AUTOCERT" \
+	-e DOCKER_ENV="$DOCKER_ENV" \
 	-p "$REGISTRY_PORT":443 \
-	"$@" \
-	-d "$image"
+	-d docker4gis/registry:"$tag"
 
 docker container run --restart always --name theregistry \
 	--network registry \
@@ -37,7 +29,7 @@ docker container run --restart always --name theregistry \
 	-e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
 	-e REGISTRY_AUTH_HTPASSWD_PATH=/var/lib/registry/htpasswd \
 	-e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
-	-e REGISTRY_HTTP_TLS_CERTIFICATE=/certificates/"$REGISTRY_HOST".crt \
-	-e REGISTRY_HTTP_TLS_KEY=/certificates/"$REGISTRY_HOST".key \
+	-e REGISTRY_HTTP_TLS_CERTIFICATE=/certificates/localhost.crt \
+	-e REGISTRY_HTTP_TLS_KEY=/certificates/localhost.key \
 	-e REGISTRY_STORAGE_DELETE_ENABLED=true \
 	-d registry:2
