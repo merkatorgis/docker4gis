e9ab464b8c92d7800658403c605311f246a10ae0
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-11-20 15:27:48 +0100

    postgis working on Debian TODO: build-args in build.sh

--- base/.plugins/runner/install.sh
+++ base/.plugins/runner/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-apk update; apk add --no-cache \
+apk update
+apk add --no-cache \
     coreutils bash
 
 HERE=$(dirname "$0")
 
 mv "$HERE/runner.sh" /usr/local/bin
-rm -rf "$HERE"
--- base/postgis/conf/onstart.sh
+++ base/postgis/conf/onstart.sh
@@ -11,10 +11,13 @@ pg.sh -c "create extension if not exists odbc_fdw"
 pg.sh -c "create extension if not exists plsh"
 pg.sh -c "create extension if not exists pgcrypto"
 pg.sh -c "create extension if not exists pgjwt"
+pg.sh -c "create extension if not exists mongo_fdw"
 
-/tmp/subconf.sh /tmp/mail/conf.sh
-/tmp/subconf.sh /tmp/web/conf.sh
+/subconf.sh /tmp/mail/conf.sh
+/subconf.sh /tmp/web/conf.sh
 
-find /tmp/conf -name "conf.sh" -exec /tmp/subconf.sh {} \;
+# This corresponds to the Dockerfile's:
+# ONBUILD COPY conf /tmp/conf
+find /tmp/conf -name "conf.sh" -exec /subconf.sh {} \;
 
 pg.sh -c "alter database ${POSTGRES_DB} set app.ddl_done to true"
