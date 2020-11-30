2fe72ed7daea7af0e019a9554ab05ddf6391276f
Wouter Scherphof <wouter.scherphof@gmail.com>
2020-11-30 15:49:03 +0100

    fix geoserver update

--- base/geoserver/build.sh
+++ base/geoserver/build.sh
@@ -4,6 +4,10 @@ IMAGE=${IMAGE:-docker4gis/$(basename "$(realpath .)")}
 DOCKER_BASE=$DOCKER_BASE
 
 mkdir -p conf
+if [ -d conf/DOCKER_USER ] && [ "$DOCKER_USER" ]; then
+    # rename template conf dir to actual DOCKER_USER value
+    mv conf/DOCKER_USER conf/"$DOCKER_USER"
+fi
 cp -r "$DOCKER_BASE"/.plugins "$DOCKER_BASE"/.docker4gis conf
 docker image build \
     -t "$IMAGE" .
