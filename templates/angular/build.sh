5256cc52719dd2cac827bbf24ed23da39558b83c
hugozalm <hugozalm@gmail.com>
2021-01-28 12:49:29 +0100

    template angular toegeveogd. Dit is nog geen generieke code maar as-is overgenomen vanuit project mapservices

---
+++ templates/angular/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wwwroot="$(pwd)/../../code/woningvoorraad"
+
+# "$wwwroot"/.npmrc provides any Nexus credentials to the builder
+if ! [ -f "$wwwroot"/.npmrc ]; then
+    # if it's not in the project source; try to get it from the user's home dir
+    [ -f ~/.npmrc ] && cp ~/.npmrc "$wwwroot"
+fi
+
+"$BASE"/build.sh "$wwwroot"
