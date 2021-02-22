d97b0adc2950b358375b849a9c6aaaef5f6b69c3
Wouter Scherphof <wouter.scherphof@gmail.com>
2021-02-22 15:17:21 +0100

    postfix: fix api mailbox @buildtime By just postponing the complete configuration to runtime.

--- base/postfix/conf/api.sh
+++ base/postfix/conf/api.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# if @buildtime, save for runtime
+if ! [ "$DOCKER_USER" ]; then
+	echo "$0" "$@" >>/onstart
+	exit
+fi
+
 user=${1}
 script=$(realpath "$2")
 shift 2
@@ -10,14 +16,11 @@ addmailbox.sh "$user"
 conf="${script}.conf"
 [ -f "${conf}" ] || echo '#!/bin/bash' >"${conf}"
 chmod +x "$conf"
+"$conf" "$user"
 
 logdir="/util/runner/log/$DOCKER_USER/$user$(dirname "${script}")"
-# if at build time, the next commands are postponed to run time, since the
-# logdir path is mapped the host; it doesn't reside inside the image
-{
-	echo mkdir -p "$logdir"
-	echo chown --recursive "$user" "$logdir"
-} >>"$conf"
+mkdir -p "$logdir"
+chown --recursive "$user" "$logdir"
 
 # see /entrypoint
 runner=$DOCKER_USER
@@ -27,14 +30,6 @@ echo "${user}   unix  -       n       n       -       -       pipe
 
 postconf -e 'transport_maps=hash:/etc/postfix/transport'
 
-if postfix status; then
-	# Run time, ${DESTINATION} known
-	echo "${user}@${DESTINATION} ${user}:" >>/etc/postfix/transport
-	postmap /etc/postfix/transport
-	"$conf" "$user"
-	postfix reload
-else
-	# Build time, ${DESTINATION} prone to change
-	echo "${user}@{{DESTINATION}} ${user}:" >>/etc/postfix/transport.template
-	echo "$conf" "$user" >>/onstart
-fi
+echo "${user}@${DESTINATION} ${user}:" >>/etc/postfix/transport
+postmap /etc/postfix/transport
+postfix reload
