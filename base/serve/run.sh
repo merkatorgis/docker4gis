fd569b7883707a0e42cc2fbdccdd4ecc91543268
Wouter Scherphof <wouter.scherphof@gmail.com>
2021-02-22 12:11:51 +0100

    serve: ensure fileport directory exists

--- base/serve/run.sh
+++ base/serve/run.sh
@@ -9,8 +9,11 @@ DOCKER_USER=$DOCKER_USER
 DOCKER_ENV=$DOCKER_ENV
 DOCKER_BINDS_DIR=$DOCKER_BINDS_DIR
 
+fileport=$DOCKER_BINDS_DIR/fileport/$DOCKER_USER
+mkdir -p "$fileport"
+
 docker container run --restart "$RESTART" --name "$CONTAINER" \
 	-e DOCKER_USER="$DOCKER_USER" \
 	--network "$DOCKER_USER" \
-	-v "$(docker4gis/bind.sh "$DOCKER_BINDS_DIR"/fileport/"$DOCKER_USER" /fileport)" \
+	-v "$(docker4gis/bind.sh "$fileport" /fileport)" \
 	-d "$IMAGE"
