bad421c5691902c8f297878a5d27f97ef0ef3e6e
Wouter Scherphof <wouter.scherphof@gmail.com>
2021-04-12 23:16:19 +0200

    posgis: dump & restore and, therefore,  upgrade

--- base/postgis/conf/onstart.sh
+++ base/postgis/conf/onstart.sh
@@ -1,35 +1,61 @@
 #!/bin/bash
 
-# clear the "last" file (see last.sh)
-echo '' >/last
-
-while ! update-postgis.sh; do
-    sleep 1
-done
-
 pg.sh -c "alter database ${POSTGRES_DB} set app.ddl_done to false"
 
-echo "Next CREATE EXTENSION command will fail for PostGIS < 3."
-echo "It's OK to ignore that error."
-pg.sh -c "create extension if not exists postgis_raster"
+dump="/fileport/$DOCKER_USER/$POSTGRES_DB" # cf dump.sh
+if ! pg.sh -c "select current_setting('app.configured')" >/dev/null 2>&1 &&
+    [ -f "$dump.roles" ] &&
+    [ -f "$dump.backup" ]; then
+    # restore dump in empty database
 
-pg.sh -c "create extension if not exists ogr_fdw"
-pg.sh -c "create extension if not exists odbc_fdw"
-pg.sh -c "create extension if not exists plsh"
-pg.sh -c "create extension if not exists pgcrypto"
-pg.sh -c "create extension if not exists pgjwt"
-pg.sh -c "create extension if not exists mongo_fdw"
+    # first restore the roles (which are not included in the backupfile)
+    pg.sh -f "$dump.roles"
 
-/subconf.sh /tmp/mail/conf.sh
-/subconf.sh /tmp/web/conf.sh
+    # prevent "schema already exists" errors
+    pg.sh -c "drop schema if exists tiger_data cascade"
+    pg.sh -c "drop schema if exists tiger cascade"
+    # restore from the backup file
+    perl "$(find / -name postgis_restore.pl 2>/dev/null)" "$dump.backup" |
+        psql -U "$POSTGRES_USER" "$POSTGRES_DB" # using pg.sh here would give search path related errors
 
-# This corresponds to the Dockerfile's:
-# ONBUILD COPY conf /tmp/conf
-find /tmp/conf -name "conf.sh" -exec /subconf.sh {} \;
+    # mark the dump files as done
+    for f in "$dump.roles" "$dump.backup" "$dump.backup.lst"; do
+        mv "$f" "$f.$(date -I'seconds')"
+    done
+else
+    # start with existing database
 
-# see last.sh
-# shellcheck disable=SC1091
-source /last
+    update-postgis.sh
+
+    echo "Next CREATE EXTENSION command will fail for PostGIS < 3."
+    echo "It's OK to ignore that error."
+    pg.sh -c "create extension if not exists postgis_raster"
+
+    pg.sh -c "create extension if not exists ogr_fdw"
+    pg.sh -c "create extension if not exists odbc_fdw"
+    pg.sh -c "create extension if not exists plsh"
+    pg.sh -c "create extension if not exists pgcrypto"
+    pg.sh -c "create extension if not exists pgjwt"
+    pg.sh -c "create extension if not exists mongo_fdw"
+
+    # clear the "last" file (see last.sh)
+    echo '' >/last
+
+    /subconf.sh /tmp/mail/conf.sh
+    /subconf.sh /tmp/web/conf.sh
+
+    # This corresponds to the Dockerfile's:
+    # ONBUILD COPY conf /tmp/conf
+    find /tmp/conf -name "conf.sh" -exec /subconf.sh {} \;
+
+    # see last.sh
+    # shellcheck disable=SC1091
+    source /last
+fi
+
+# this setting is used in the test above (the value doesn't matter; it just
+# needs to be set)
+pg.sh -c "alter database $POSTGRES_DB set app.configured to true"
 
 # enable the safeupdate extension
 # https://github.com/eradman/pg-safeupdate
