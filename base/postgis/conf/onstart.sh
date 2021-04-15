0affaeceea4fde33a171be1d5154e33c7fc3bd4c
Wouter Scherphof <wouter.scherphof@gmail.com>
2021-04-15 11:20:28 +0200

    postgis: refactor provisioning code

--- base/postgis/conf/onstart.sh
+++ base/postgis/conf/onstart.sh
@@ -4,22 +4,23 @@
 pg.sh -c "alter database ${POSTGRES_DB} set app.ddl_done to false"
 
 # see dump_restore
-if ! restore; then
+time if ! restore; then
     # we didn't restore an existing dump in an unprovisioned database, so we're
-    # either provisioning a new database from its own DDL, or (possibly)
-    # updating an existing database with any new DDL
+    # either provisioning a new database from its own DDL, or updating an
+    # existing database with any new DDL
 
-    # maybe this image contains an newer (minor) version of PostGIS than the
-    # last image that served this database
+    # maybe this image contains an newer (minor, updatable without dump &
+    # restore) version of PostGIS than the last image that served this database
     update-postgis.sh
 
     extension() {
         pg.sh -c "create extension if not exists $1"
     }
 
-    echo "Next CREATE EXTENSION command will fail for PostGIS < 3."
-    echo "It's OK to ignore that error."
-    extension postgis_raster
+    # POSTGIS_MAJOR is eg. 2.5 or 3.1
+    postgis_major_major=$(echo "$POSTGIS_MAJOR" | cut -d'.' -f1)
+    # from PostGIS 3, postgis_raster is a separate extension
+    [ "$postgis_major_major" -ge 3 ] && extension postgis_raster
 
     extension ogr_fdw
     extension odbc_fdw
