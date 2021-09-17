10d700ab1bb1725c75093a42d92ae1884bc00ca3
Wouter Scherphof <wouter.scherphof@gmail.com>
2021-09-17 11:07:27 +0200

    bats: ensure submodules

--- base/test_helper/load.bash
+++ base/test_helper/load.bash
@@ -5,6 +5,9 @@
 base=$(basename "$BATS_TEST_FILENAME" .bats)
 cmd="$BATS_TEST_DIRNAME"/"$base"
 
+git -C "$DOCKER_BASE/.." submodule init
+git -C "$DOCKER_BASE/.." submodule update
+
 load "$DOCKER_BASE"/test_helper/bats-support/load.bash
 load "$DOCKER_BASE"/test_helper/bats-assert/load.bash
 load "$DOCKER_BASE"/test_helper/bats-file/load.bash
