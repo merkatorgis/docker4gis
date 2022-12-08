12ea6699987d5dedfc7819ac746b1e1d3c77de54
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-12-08 08:53:39 +0100

    camptocamp

---
+++ base/qgis/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+IMAGE=$IMAGE
+CONTAINER=$CONTAINER
+RESTART=$RESTART
+
+DOCKER_USER=$DOCKER_USER
+DOCKER_ENV=$DOCKER_ENV
+DOCKER_BINDS_DIR=$DOCKER_BINDS_DIR
+
+docker container run --restart "$RESTART" --name "$CONTAINER" \
+	-e DOCKER_USER="$DOCKER_USER" \
+	--network "$DOCKER_USER" \
+	"$@" \
+	-d "$IMAGE"
