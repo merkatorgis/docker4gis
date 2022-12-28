3af89f11ea2380faf6dbd9289cb6bfc1ca475904
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-12-28 12:24:08 +0100

    wip qgis: upload skeleton working

--- base/qgis/conf/upload/index.php
+++ base/qgis/conf/upload/index.php
@@ -1,20 +1,77 @@
 <html>
 
-<head></head>
+<head>
+  <style>
+    body {
+      overflow: hidden;
+      margin: 1em;
+    }
+
+    h1 {
+      height: 30px;
+      margin: 0;
+    }
+
+    #box {
+      display: flex;
+      margin-top: 1em;
+      height: calc(100% - 30px - 1em);
+    }
+
+    nav {
+      flex-grow: 1;
+      padding-left: 1em;
+    }
+
+    label>input {
+      width: 100%;
+      max-width: 20em;
+      margin-top: .25em;
+    }
+  </style>
+</head>
 
 <body>
-  <h4>File uploads</h4>
-  <form enctype="multipart/form-data" action="upload.php" method="post">
-    <p>
-      Select File:
-      <input type="file" size="35" name="uploadedfile" />
-      <br />
 
-      Destination <input type="text" size="35" name="destination" value="<?php echo "value"; ?>" />
-      <br />
-      <input type="submit" name="Upload" value="Upload" />
-    </p>
-  </form>
+  <h1>Upload file to QGIS Server</h1>
+
+  <div id="box">
+
+    <div>
+      <form enctype="multipart/form-data" action="upload.php" method="post">
+        <p>
+          <label>
+            Select File<br />
+            <input type="file" name="file" id="file" required />
+          </label>
+        </p>
+
+        <p>
+          <label>
+            Project (if not .qgs)<br />
+            <input type="text" name="project" id="project" value="<?php echo $_REQUEST['project']; ?>" />
+          </label>
+        </p>
+
+        <p>
+          <input type="submit" name="upload" id="upload" value="Upload" />
+        </p>
+      </form>
+
+      <script>
+        file.addEventListener('change', () => {
+          project.disabled = file.value.endsWith('qgs') || file.value.endsWith('qgz');
+        });
+      </script>
+    </div>
+
+    <nav>
+      <iframe id="iframe" width="100%" height="100%" src="../files/qgis/<?php echo $_REQUEST['project']; ?>">
+      </iframe>
+    </nav>
+
+  </div>
+
 </body>
 
 </html>
--- base/qgis/conf/upload/upload.php
+++ base/qgis/conf/upload/upload.php
@@ -1,7 +1,31 @@
- 
 <?php
 
-// Show all information, defaults to INFO_ALL
-phpinfo();
+// $_FILES['file']
+// Array
+// (
+//     [name] => 65521-1.qgz
+//     [type] => application/octet-stream
+//     [tmp_name] => /tmp/php8HjLks
+//     [error] => 0
+//     [size] => 3987
+// )
+$file = $_FILES['file'];
 
-?>
+$path_parts = pathinfo($file['name']);
+
+if ($path_parts['extension'] == 'qgs' || $path_parts['extension'] == 'qgz') {
+    $project = $path_parts['filename'];
+} else {
+    $project = $_REQUEST['project'];
+}
+
+$dir = "/fileport/files/qgis/$project/";
+mkdir($dir, 0777, true);
+
+$dest = $dir . $path_parts['basename'];
+
+if (move_uploaded_file($file['tmp_name'], $dest)) {
+    echo "The file $dest has been uploaded";
+} else {
+    echo "There was an error uploading the file, please try again!";
+}
