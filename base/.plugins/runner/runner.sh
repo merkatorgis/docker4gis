f7e59785485870ac794b1b018b63fda662731ad2
Wouter Scherphof <wouter.scherphof@gmail.com>
2023-10-10 15:18:26 +0200

    cosmetic

--- base/.plugins/runner/runner.sh
+++ base/.plugins/runner/runner.sh
@@ -9,17 +9,17 @@ DOCKER_USER=${DOCKER_USER:-$(basename "$0")}
 script=$(realpath "$1")
 shift 1
 
-log="/util/runner/log/${DOCKER_USER}/$(whoami)${script}.$(date -I).log"
-mkdir -p "$(dirname "${log}")"
+log="/util/runner/log/$DOCKER_USER/$(whoami)$script.$(date -I).log"
+mkdir -p "$(dirname "$log")"
 
 # Preserve quoted arguments with spaces; see
 # https://www.gnu.org/software/bash/manual/bash.html#index-printf.
 args=$(printf '%q ' "$@")
-action="${script} $$ $args"
+action="$script $$ $args"
 
-echo "$$ > $(date -Ins)" >>"${log}"
-"$action" >>"${log}" 2>&1
+echo "$$ > $(date -Ins)" >>"$log"
+"$action" >>"$log" 2>&1
 err="$?"
 
-echo "$$ < $(date -Ins)" >>"${log}"
-exit "${err}"
+echo "$$ < $(date -Ins)" >>"$log"
+exit "$err"
