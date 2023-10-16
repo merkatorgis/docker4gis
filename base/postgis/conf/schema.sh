ad6d310fe01733eccedb68cbe5c2ca7fb6b7c78a
Wouter Scherphof <wouter.scherphof@gmail.com>
2023-10-16 17:07:21 +0200

    postgis: refactor of schema.sh

--- base/postgis/conf/schema.sh
+++ base/postgis/conf/schema.sh
@@ -1,62 +1,56 @@
 #!/bin/bash
-set -e
+# set -x
 
 scripts_dir=$1
-export SCHEMA=${2:-$(basename ${scripts_dir})}
+export SCHEMA=${2:-$(basename "$scripts_dir")}
 
-if [ $(pg.sh -Atc "
-		select count(*)
-		from information_schema.routines
-		where routine_name = '__version'
-		and routine_schema = '${SCHEMA}'
-	 ") = 0 ]; then
-	current=0
-	pg.sh -c "create schema if not exists ${SCHEMA}"
-	if [ "${SCHEMA}" != mail -a "${SCHEMA}" != web ]; then
-		pg.sh -c "grant usage on schema ${SCHEMA} to web_anon"
-		pg.sh -c "grant usage on schema ${SCHEMA} to web_passwd"
-		pg.sh -c "grant usage on schema ${SCHEMA} to web_user"
-		pg.sh -c "grant usage on schema ${SCHEMA} to web_admin"
-	fi
+pushd "$scripts_dir" || exit 1
+
+# The optional 0-script should always be run.
+[ -x 0.sh ] && {
+	# If it exists, it should complete successfully.
+	./0.sh || exit 1
+}
+
+# Test if versioning function exists.
+function=$SCHEMA.__version
+if pg.sh -c "select '$function'::regproc" >/dev/null 2>&1; then
+	# Read the current version from the database.
+	version=$(pg.sh -Atc "select $function()") || exit 1
 else
-	current=$(pg.sh -Atc "select ${SCHEMA}.__version()")
-fi
-
-update() {
-	pg.sh -c "
-		CREATE OR REPLACE FUNCTION ${SCHEMA}.__version()
-		RETURNS integer
-		LANGUAGE plpgsql
-		AS \$\$
-		BEGIN
-		    RETURN ${current};
-		END;
-		\$\$;
+	version=0
+	# Create the schema.
+	pg.sh -c "create schema if not exists $SCHEMA"
+	[ "$SCHEMA" != mail ] && [ "$SCHEMA" != web ] && [ "$SCHEMA" != admin ] && pg.sh -c "
+		grant usage on schema $SCHEMA
+		to web_anon
+		, web_passwd
+		, web_user
+		, web_admin
 	"
-	if [ "${current}" = 1 ]; then
-		pg.sh -c "
-			COMMENT ON FUNCTION ${SCHEMA}.__version() IS \$\$
-				Returns the version number of this schema's data model,
-				for the migration scripts to know where the should start.
-			\$\$;
-		"
-	fi
-}
-
-next() {
-	echo $((current + 1))
-}
-
-pushd "${scripts_dir}"
-
-if [ -f 0.sh ]; then
-	./0.sh
 fi
 
-while [ -f "$(next)".sh ]; do
-	current=$(next)
-	"./${current}.sh"
-	update
+proceed() {
+	# Increment version.
+	version=$((version + 1))
+	script=./"$version".sh
+	# If a script to create the new version exists, execute it.
+	[ -x "$script" ] &&
+		"$script"
+}
+
+while proceed; do
+	# A script for the next version existed, and was executed successfully. Now,
+	# ensure the database knows its current version.
+	pg.sh -c "
+		CREATE OR REPLACE FUNCTION $function()
+		RETURNS integer
+		LANGUAGE sql AS \$function\$
+			-- Returns the version number of this schema's data model,
+			-- for the migration scripts to know where they should start.
+		    SELECT $version;
+		\$function\$;
+	"
 done
 
-popd
+popd || exit 1
