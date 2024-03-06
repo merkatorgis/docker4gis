fd1d70e85ecf5001fd7a04bf6f893454043de736
Wouter Scherphof <wouter.scherphof@gmail.com>
2024-03-06 11:09:23 +0100

    cron: remove debug logging

--- base/cron/conf/cron.sh
+++ base/cron/conf/cron.sh
@@ -34,13 +34,7 @@ lock=$dir/$file
 job=$lock.job
 
 echo "#!/bin/bash" >"$job"
-log=$lock.log
-echo "
-	echo >> '$log'
-	echo \$0 \$@ >> '$log'
-	env >> '$log'
-" >>"$job"
-echo "runner.sh '$script' $* >> '$log'" >>"$job"
+echo "runner.sh '$script' $*" >>"$job"
 chmod +x "$job"
 
 # Use flock to prevent duplicate cron jobs. Each job gets a unique script and
