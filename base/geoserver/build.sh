31daf07cbaf74e5c571dcef70c608760db68a401
Wouter Scherphof <wouter.scherphof@gmail.com>
2024-03-11 14:55:41 +0100

    geoserver: use bin image

--- base/geoserver/build.sh
+++ base/geoserver/build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+GEOSERVER_VERSION='2.24.2'
+GEOSERVER_EXTENSIONS='css printing pyramid'
+
 IMAGE=${IMAGE:-docker4gis/$(basename "$(realpath .)")}
 DOCKER_BASE=$DOCKER_BASE
 
 mkdir -p conf
 if [ -d conf/DOCKER_USER ] && [ "$DOCKER_USER" ]; then
-    # rename template conf dir to actual DOCKER_USER value
+    # Rename template conf dir to actual DOCKER_USER value.
     mv conf/DOCKER_USER conf/"$DOCKER_USER"
 fi
 cp -r "$DOCKER_BASE"/.plugins "$DOCKER_BASE"/.docker4gis conf
 docker image build \
+    --build-arg GEOSERVER_VERSION="$GEOSERVER_VERSION" \
+    --build-arg GEOSERVER_EXTENSIONS="$GEOSERVER_EXTENSIONS" \
     --build-arg GEOSERVER_EXTRA_EXTENSIONS="$GEOSERVER_EXTRA_EXTENSIONS" \
     -t "$IMAGE" .
 rm -rf conf/.plugins conf/.docker4gis
--- base/geoserver/conf/geoserver/extensions.sh
+++ base/geoserver/conf/geoserver/extensions.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-extensions=$*
+GEOSERVER_DOWNLOAD_URL=https://sourceforge.net/projects/geoserver/files/GeoServer/$GEOSERVER_VERSION
 
-for extension in $extensions; do
+for extension in $GEOSERVER_EXTENSIONS $GEOSERVER_EXTRA_EXTENSIONS; do
     zip=geoserver-$GEOSERVER_VERSION-$extension-plugin.zip
-    wget -O "$zip" "$GEOSERVER_DOWNLOAD_URL/extensions/$zip/download"
-    unzip -qo "$zip" -d /tmp/conf/webapps/geoserver/WEB-INF/lib
+    file=$GEOSERVER_BIN/$zip
+    [ -f "$file" ] || wget -O "$file" "$GEOSERVER_DOWNLOAD_URL/extensions/$zip/download"
+    unzip -o "$file" -d /tmp/conf/webapps/geoserver/WEB-INF/lib
 done
