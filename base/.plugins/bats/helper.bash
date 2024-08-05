d59d7d272bf9efb161379b942e5cf7e732b0101f
Wouter Scherphof <wouter.scherphof@gmail.com>
2024-08-05 09:26:58 +0200

    fix bats_load_library

--- base/.plugins/bats/helper.bash
+++ base/.plugins/bats/helper.bash
@@ -3,12 +3,13 @@
 # #!/usr/bin/env bats
 # load ~/.bats/helper.bash
 
-# https://www.shellcheck.net/wiki/SC2044
+# Load the bats libs for which we included the sources here, in the absence of a
+# proper npm package for them. See https://www.shellcheck.net/wiki/SC2044 for
+# the loop over `find`.
 while IFS= read -r -d '' lib; do
-    lib=$(basename "$lib")
-    # cf. https://github.com/drevops/bats-helpers?tab=readme-ov-file#usage
-    bats_load_library "$lib"
-done < <(find "$BATS_LIB_PATH" -type d -name "bats-*-*" -print0)
+    load "$lib"/load.bash
+done < <(find "$DOCKER_BASE/.plugins/bats" -type d -name "bats-*-*" -print0)
 
+# Set the cmd variable.
 base=$(basename "$BATS_TEST_FILENAME" .bats)
 cmd="$BATS_TEST_DIRNAME"/"$base"
--- base/.plugins/bats/install.sh
+++ base/.plugins/bats/install.sh
@@ -4,5 +4,3 @@ here=$(dirname "$0")
 
 mkdir -p ~/.bats
 cp "$here"/*.bash ~/.bats
-
-[ "$BATS_LIB_PATH" ] && cp -r "$here"/bats-*-* "$BATS_LIB_PATH"
