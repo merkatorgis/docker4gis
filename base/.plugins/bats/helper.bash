5b40cce0b316bdd2f08987535d4afe2459135b99
Wouter Scherphof <wouter.scherphof@gmail.com>
2024-08-07 21:19:24 +0200

    bats: helper(), $cmd -> $CMD, dg bats

--- base/.plugins/bats/helper.bash
+++ base/.plugins/bats/helper.bash
@@ -1,7 +1,10 @@
-# Start any bats file with:
-#
-# #!/usr/bin/env bats
 # load ~/.bats/helper.bash
+#
+# function setup_file() {
+#     helper
+# }
+
+# Start any bats file with the lines above.
 
 # Load the bats libs for which we included the sources here, in the absence of a
 # proper npm package for them. See https://www.shellcheck.net/wiki/SC2044 for
@@ -10,6 +13,8 @@ while IFS= read -r -d '' lib; do
     load "$lib"/load.bash
 done < <(find "$DOCKER_BASE/.plugins/bats" -type d -name "bats-*-*" -print0)
 
-# Set the cmd variable.
-base=$(basename "$BATS_TEST_FILENAME" .bats)
-cmd="$BATS_TEST_DIRNAME"/"$base"
+helper() {
+    # Set the CMD variable.
+    base=$(basename "$BATS_TEST_FILENAME" .bats)
+    export CMD="$BATS_TEST_DIRNAME"/"$base"
+}
