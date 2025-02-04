8daab9552b65b93387b1eb0dbabf3361463628a1
Azure Pipeline <pipeline@azure.com>
2025-02-04 17:06:22 +0100

    devops: permit pipelines to use deployment resources

--- devops/conf/pipelines.sh
+++ devops/conf/pipelines.sh
@@ -44,6 +44,48 @@ for yaml in \
 
     build_definition_id=$(node --print "($build_definition).id")
 
+    pipelinePermissions() {
+        local resource_type=$1
+        local resource_id=$2
+
+        log "Permit pipeline $name to use $resource_type $environment"
+
+        /devops/rest.sh project PATCH \
+            "pipelines/pipelinePermissions/$resource_type/$resource_id" '' "{
+                \"pipelines\": [{
+                    \"id\": $build_definition_id,
+                    \"authorized\": true
+                }]
+            }"
+    }
+
+    get_object_id_by_name() {
+        local path=$1
+        local parameter_name=$2
+        object=$(/devops/rest.sh project GET "$path" \
+            "$parameter_name=$environment")
+        node --print "($object).value[0].id"
+    }
+
+    # Permit the continuous integration pipeline to use the deployment
+    # environments and service connections.
+    [ "$PR" ] || {
+        for environment in TEST PRODUCTION; do
+            # Find the service connection id by name.
+            id=$(get_object_id_by_name serviceendpoint/endpoints endpointNames)
+            # Permit the pipeline to use the service connection.
+            pipelinePermissions endpoint "$id"
+
+            # Different environment for non-package repositories.
+            [ "$REPOSITORY" = ^package ] || environment+=_SINGLE
+
+            # Find the environment id by name.
+            id=$(get_object_id_by_name pipelines/environments name)
+            # Permit the pipeline to use the environment.
+            pipelinePermissions environment "$id"
+        done
+    }
+
     # Create a policy to require a successful build before merging.
     [ "$PR" ] && {
         log Create build policy "$name"
