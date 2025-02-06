e301cd8bfa0907771aa70287a4eb5684adee4cbd
Azure Pipeline <pipeline@azure.com>
2025-02-06 11:40:11 +0100

    devops: fix DOCKER4GIS_VERSION, run set outside container, shorter timeout environment

--- devops/run.sh
+++ devops/run.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 
-DOCKER_REPO=$(basename "$(realpath "$(dirname "$0")")")
+here=$(realpath "$(dirname "$0")")
+
+DOCKER_REPO=$(basename "$here")
 IMAGE=docker4gis/$DOCKER_REPO
 CONTAINER=docker4gis-$DOCKER_REPO
 
-here=$(realpath "$(dirname "$0")")
-docker4gis_dir=$here/conf/docker4gis
-mkdir -p "$docker4gis_dir"
-
-cp -r "$here"/../* "$docker4gis_dir"
-docker image build -t "$IMAGE" "$(dirname "$0")"
-rm -rf "$docker4gis_dir"
-
-ENV_FILE=$HOME/.$CONTAINER.env
+export ENV_FILE=$HOME/.$CONTAINER.env
 touch "$ENV_FILE"
 chown "$USER" "$ENV_FILE"
 chmod 600 "$ENV_FILE"
 
-docker_socket=/var/run/docker.sock
+case "$1" in
+set | s)
+	shift
+	exec "$here"/conf/set.sh "$@"
+	;;
+components | c)
+	shift
+	;;
+esac
+
+# Set default action.
+set -- components "$@"
+
+# Copy the docker4gis tool into the conf directory, so that it can be included
+# in the image.
+docker4gis_dir=$here/conf/docker4gis
+rm -rf "$docker4gis_dir" 2>/dev/null
+mkdir "$docker4gis_dir"
+find "$here"/.. -maxdepth 1 \
+	! -name ".*" \
+	! -name node_modules \
+	! -name devops \
+	-exec cp -r {} "$docker4gis_dir" \;
+
+# Build the image.
+docker image build -t "$IMAGE" "$(dirname "$0")"
+
+# Clean up.
+rm -rf "$docker4gis_dir"
 
 find_docker_user() {
 	while read -r env_file; do
@@ -39,15 +61,8 @@ find_docker_user() {
 [ -n "$DOCKER_USER" ] ||
 	find_docker_user
 
-# Set default action.
-case "$1" in
-components | c | set | s)
-	true
-	;;
-*)
-	set -- components "$@"
-	;;
-esac
+docker_socket=/var/run/docker.sock
+container_env_file=/devops/env_file
 
 # Tee all stdout & stderr to a log file (from
 # https://superuser.com/a/212436/462952).
@@ -64,6 +79,7 @@ docker container run --name "$CONTAINER" \
 	--env DEVOPS_ORGANISATION="$DEVOPS_ORGANISATION" \
 	--env DEVOPS_DOCKER_REGISTRY="$DEVOPS_DOCKER_REGISTRY" \
 	--env DEVOPS_VPN_POOL="$DEVOPS_VPN_POOL" \
-	--mount type=bind,source="$ENV_FILE",target=/devops/env_file \
+	--env ENV_FILE="$container_env_file" \
+	--mount type=bind,source="$ENV_FILE",target="$container_env_file" \
 	--mount type=bind,source="$docker_socket",target="$docker_socket" \
 	"$IMAGE" "$@"
